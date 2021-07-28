6936357bad93f55fa778442e490ae2148d13d167
Jordan Race <diamondhojo@gmail.com>
2021-07-28 15:55:08 -0400

    Created the actual snakes and ladders system; big fixes

--- backend.js
+++ backend.js
@@ -2,36 +2,41 @@
 var bsize = 10;
 var snakes = []
 var ladders = []
-var numSnakesLadders = 10 //number of snakes and ladders on board at one time (each)
-var turn = "p1"
+var numSnakesLadders = 8 //number of snakes and ladders on board at one time (each)
+var player = 1
 var p1 = 0
 var p2 = 0
 var items;
 
 function resetGame(){
-    p1 = 0
-    p2 = 0
-    snakes = []
-    ladders = []
-    document.getElementById("roll").disabled = false;
+    location.reload()
+}
 
-    for(var i = 0; i < items.length; i++){
-        items[i].style.backgroundColor = "white"
-    }
-    
-    generateSnakesLadders();
+function random(min, max){
+    return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
 function roll(){
     number = Math.floor((Math.random() * 6) + 1);
     
-    if(turn == "p1"){
+    if(player == 1){
         var oldPos = 1
         if(p1 > 0){ //p1 > 0 prevents uncaught type error (oldPos can't be 0 whenever it's called)
             oldPos = p1 //temporary storage of last positon
         }
         
         p1 += number
+        if(snakes.includes(p1)){
+            p1 = p1-+ 10
+            console.log("snakes")
+            document.getElementById("label").innerHTML = "Snake! -10pts"
+        } else if(ladders.includes(p1)){
+            p1 = p1 + 10
+            console.log("ladder")
+            document.getElementById("label").innerHTML = "Ladder! +10pts"
+        } else {
+            document.getElementById("label").innerHTML = ""
+        }
 
         if(p1 == p2){ //same spot
             document.getElementById(p1).style.backgroundColor = "green";
@@ -56,14 +61,26 @@ function roll(){
             }
         }
         
-        turn = "p2"
-    } else if(turn = "p2"){
+        player = 2
+    } else if(player = 2){
         var oldPos = 1
         if(p2 > 0){
             oldPos = p2
         }
         
         p2 += number
+
+        if(snakes.includes(p2)){
+            p2 = p2-+ 10
+            console.log("snakes")
+            document.getElementById("label").innerHTML = "Snake! -10pts"
+        } else if(ladders.includes(p2)){
+            p2 = p2 + 10
+            console.log("ladder")
+            document.getElementById("label").innerHTML = "Ladder! +10pts"
+        } else {
+            document.getElementById("label").innerHTML = ""
+        }
 
         if(p1 == p2){
             document.getElementById(p2).style.backgroundColor = "green";
@@ -88,43 +105,8 @@ function roll(){
             }
         }
         
-        turn = "p1"
+        player = 1
     }
-}
-
-function generateSnakesLadders(){
-    for(var i=0; i<10; i++){
-        while(snakes.length <= numSnakesLadders){
-            var snake = Math.floor((Math.random() * (100-30)+30));
-            if(snakes.includes(snake)){ //duplicate, generating new number
-                while(snakes.includes(snake)){ //keeps regenerating until snake is not already in snakes
-                    snake = Math.floor((Math.random() * (70-1)+1));
-                }
-                snakes.push(snake)
-                break;
-            } else {
-                snakes.push(snake)
-                break;
-            }
-        }
-
-        while(ladders.length <= numSnakesLadders){
-            var ladder = Math.floor((Math.random() * (70-1)+1));
-            if(snakes.includes(ladder) | ladders.includes(ladder)){
-                while(snakes.includes(ladder) | ladders.includes(ladder)){
-                    ladder = Math.floor((Math.random() * (70-1)+1));
-                }
-                ladders.push(ladder)
-                break;
-            } else {
-                ladders.push(ladder)
-                break;
-            }
-        }
-    }
-
-    console.log("Snakes (" + snakes.length + "): " + snakes)
-    console.log("Ladders (" + ladders.length + "): " + ladders)
 }
 
 function generate() {
@@ -144,7 +126,39 @@ function generate() {
 
     items = document.querySelectorAll(".grid-item")
 
-    generateSnakesLadders();
+    for(var i=0; i<numSnakesLadders; i++){
+        while(snakes.length <= numSnakesLadders){
+            var snake = random(11, 99) //between 11 and 99 (both inclusively)
+            if(snakes.includes(snake)){ //duplicate, generating new number
+                while(snakes.includes(snake)){ //keeps regenerating until snake is not already in snakes
+                    snake = random(11, 99)
+                }
+                //items[snake].style.backgroundColor = "red" //for testing; shows location of each snake/ladder
+                snakes.push(snake)
+                break;
+            } else {
+                //items[snake].style.backgroundColor = "red"
+                snakes.push(snake)
+                break;
+            }
+        }
+
+        while(ladders.length <= numSnakesLadders){
+            var ladder = random(1, 89) //between 1 and 99 (both inclusively)
+            if(snakes.includes(ladder) | ladders.includes(ladder)){
+                while(snakes.includes(ladder) | ladders.includes(ladder)){
+                    ladder = random(1, 89)
+                }
+                //items[ladder].style.backgroundColor = "blue"
+                ladders.push(ladder)
+                break;
+            } else {
+                //items[ladder].style.backgroundColor = "blue"
+                ladders.push(ladder)
+                break;
+            }
+        }
+    }
 
     console.log("Snakes (" + snakes.length + "): " + snakes)
     console.log("Ladders (" + ladders.length + "): " + ladders)
